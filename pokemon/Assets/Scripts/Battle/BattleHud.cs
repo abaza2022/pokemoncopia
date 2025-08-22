2f2772353cc537421e158931a99ec49336488bf0
Brayan Steven León Martinez <brot10102017@gmail.com>
2025-08-21 21:49:01 -0500

    avance

---
+++ pokemon/Assets/Scripts/Battle/BattleHud.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHud : MonoBehaviour
+{
+    [SerializeField] Text nameText;
+    [SerializeField] Text levelText;
+    [SerializeField] HPBar hpBar;
+
+    
+}
